31614621ccba27c9020fbc77df5c4f01568fbc9b
Cheng Deng <dc0210@gmail.com>
2013-10-31 15:47:02 -0400

    clean code:

--- app/js/config.js
+++ app/js/config.js
@@ -5,14 +5,3 @@ angular.module('myApp.config', [])
 
    // your Firebase URL goes here
    .constant('FBURL', 'https://ux-survey.firebaseio.com');
-
-
-/*********************
- * !!FOR E2E TESTING!!
- *
- * Must enable email/password logins and manually create
- * the test user before the e2e tests will pass
- *
- * user: test@test.com
- * pass: test123
- */
--- app/js/services.js
+++ app/js/services.js
@@ -43,34 +43,7 @@
                   }
                }
             }
-         }])
 
-      .factory('profileCreator', ['Firebase', 'FBURL', '$rootScope', function(Firebase, FBURL, $rootScope) {
-         return function(id, email, callback) {
-            new Firebase(FBURL).child('users/'+id).set({email: email, name: firstPartOfEmail(email)}, function(err) {
-               //err && console.error(err);
-               if( callback ) {
-                  callback(err);
-                  $rootScope.$apply();
-               }
-            });
-
-            function firstPartOfEmail(email) {
-               return ucfirst(email.substr(0, email.indexOf('@'))||'');
-            }
-
-            function ucfirst (str) {
-               // http://kevin.vanzonneveld.net
-               // +   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-               // +   bugfixed by: Onno Marsman
-               // +   improved by: Brett Zamir (http://brett-zamir.me)
-               // *     example 1: ucfirst('kevin van zonneveld');
-               // *     returns 1: 'Kevin van zonneveld'
-               str += '';
-               var f = str.charAt(0).toUpperCase();
-               return f + str.substr(1);
-            }
-         }
       }]);
 
    function errMsg(err) {
